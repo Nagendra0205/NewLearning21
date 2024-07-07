2fc6edb8dcc6d258d4249518fe9b2d57d6d03e88
kakarlanagendrakumar@gmail.com <nag@gmail.com>
2024-07-07 19:10:14 +0530

    first Commit

---
+++ array.js
@@ -0,0 +1,40 @@
+var num =[];
+for(let i=0; i<100;i++)
+{
+    if(i%2==0){
+        console.log(i);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var name="Hello word";
+console.log(name)
+const age=[10,20,30,40,50];
+const Persons=['Nag','Vamsi','Ram']
+console.log(age[0]);
+console.log(age.length);
+/*for(i=0;i<age.length;i++){
+    console.log(age[i]);
+}
+for(items in age){
+    console.log(age[items]);
+}
+for (dots of age){
+    console.log(dots)
+}
+Persons.forEach((item)=>{
+    console.log(item)
+})
+*/
